c80a05848c7b0f9e7d692d751edc1af489842d1b
MaorShmueli55 <maorshmueli5@gmail.com>
2025-01-07 13:30:39 +0200

    11

--- clinet/src/components/Filter.tsx
+++ clinet/src/components/Filter.tsx
@@ -1,39 +1,52 @@
-import React from 'react'
+import React from "react";
 import { ImHome } from "react-icons/im";
 import { IoFlagSharp } from "react-icons/io5";
 import { HiMiniTrophy } from "react-icons/hi2";
 import { FaTshirt } from "react-icons/fa";
-import player from "../assets/img/bc-players.svg"
-
+import player from "../assets/img/bc-players.svg";
+import flag from "../assets/img/bc-countries.svg";
+import cup from "../assets/img/bc-competitions.svg";
+import { RxDoubleArrowRight } from "react-icons/rx";
 
 const Filter = () => {
   return (
-    <div className='bg-[#fff] h-[50px] p-[10px]'>
-      <div className='flex gap-[7px]'>
-        <div><ImHome /></div>
-        <div className='flex'>
-          <div><IoFlagSharp /></div>
-        <div></div>
-        <div></div>
+    <div className="bg-[#fff] h-[55px] p-[10px]">
+      <div className="flex gap-[15px] item-center">
+        <div className="h-[35px] w-[35px] bg-[#e9e9e9] flex justify-center items-center">
+          <ImHome className="text-[#00193f] text-[20px]" />
         </div>
-        <div>
-          <div><HiMiniTrophy /></div>
-        <div></div>
-        <div></div>
+        <div className="flex">
+          <div className=" bg-[#00193f] h-[35px] w-[35px]  text-[#fff] flex justify-center items-center">
+          <img className="" src={flag} alt="player" />
+          </div>
+          <div className="text-[#1A3151] font-bold text-[12px] bg-[#F2F2F2] h-[35px] p-[10px] flex justify-center items-center">Country</div>
+          <button className="bg-[#DDDDDD] h-[35px] w-[25px] flex justify-center items-center text-[18px] text-[#0EB1EE] hover:bg-[#0EB1EE] hover:text-[#DDDDDD]"><RxDoubleArrowRight /></button>
         </div>
-        <div>
-          <div><FaTshirt /></div>
-        <div></div>
-        <div></div>
+        <div className="flex">
+          <div className=" bg-[#00193f] h-[35px] w-[35px]  text-[#fff] flex justify-center items-center">
+          <img className="" src={cup} alt="player" />
+          </div>
+          <div className="text-[#1A3151] font-bold text-[12px] bg-[#F2F2F2] h-[35px] p-[10px] flex justify-center items-center">Competition</div>
+          <button className="bg-[#DDDDDD] h-[35px] w-[25px] flex justify-center items-center text-[18px] text-[#0EB1EE] hover:bg-[#0EB1EE] hover:text-[#DDDDDD]"><RxDoubleArrowRight /></button>
         </div>
-        <div>
-          <div className='bg-black'><img className='' src={player} alt="player" />bh</div>
-        <div></div>
-        <div></div>
+        <div className="flex">
+          <div className=" bg-[#00193f] h-[35px] w-[35px]  text-[#fff] flex justify-center items-center">
+            <FaTshirt />
+          </div>
+          <div className="text-[#1A3151] font-bold text-[12px] bg-[#F2F2F2] h-[35px] p-[10px] flex justify-center items-center">Club</div>
+          <button className="bg-[#DDDDDD] h-[35px] w-[25px] flex justify-center items-center text-[18px] text-[#0EB1EE] hover:bg-[#0EB1EE] hover:text-[#DDDDDD]"><RxDoubleArrowRight /></button>
         </div>
-      </div>
-      </div>
-  )
-}
+        <div className="flex">
+          <div className=" bg-[#00193f] h-[35px] w-[35px]  text-[#fff] flex justify-center items-center">
+          <img className="" src={player} alt="player" />
+          </div>
+          <div className="text-[#1A3151] font-bold text-[12px] bg-[#F2F2F2] h-[35px] p-[10px] flex justify-center items-center">Player</div>
+          <button className="bg-[#DDDDDD] h-[35px] w-[25px] flex justify-center items-center text-[18px] text-[#0EB1EE] hover:bg-[#0EB1EE] hover:text-[#DDDDDD]"><RxDoubleArrowRight /></button>
+        </div>
 
-export default Filter
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
