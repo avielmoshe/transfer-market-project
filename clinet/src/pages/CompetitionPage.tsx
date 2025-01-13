a04ba6a22b177fd93baec80661e01e5d577c3cd7
avielmoshe <mosheavieli@gmail.com>
2025-01-13 17:07:42 +0200

    1

--- clinet/src/pages/CompetitionPage.tsx
+++ clinet/src/pages/CompetitionPage.tsx
@@ -1,5 +1,38 @@
+import DynamicHeader from "@/components/HeaderForProfile";
+import { fetchDataOfOneComRow } from "@/utils/api";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+
 function CompetitionPage() {
-  return <div>CompetitionPage</div>;
+  const { id } = useParams();
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["DataOfOneCom", { id }],
+    queryFn: () => fetchDataOfOneComRow(id),
+  });
+
+  if (error instanceof Error) return null;
+  if (!data) {
+    return null;
+  }
+  console.log(data);
+  const dataForHeader = {
+    type: "competition",
+    title: data.competition.competitionName,
+    frontImg: data.competition.competitionImage,
+    secondImg: data.competition.competitionCountryImage,
+    secondTitle: data.competition.competitionCountryName,
+    marketValue:
+      data.competition.marketValueCurrency +
+      data.competition.marketValue +
+      data.competition.marketValueNumeral,
+    firstArray: [{}, {}, {}],
+  };
+  return (
+    <>
+      <DynamicHeader dataForHeader={dataForHeader} />
+    </>
+  );
 }
 
 export default CompetitionPage;
