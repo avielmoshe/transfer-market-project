78aa32d0ce0bdde88bfab9d4df6700242312fd5f
MaorShmueli55 <maorshmueli5@gmail.com>
2025-01-16 18:57:55 +0200

    15

--- clinet/src/components/FootballFeild.tsx
+++ clinet/src/components/FootballFeild.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { TbPlayFootball } from 'react-icons/tb';
 
-// טיפוס למיקום שחקן
+
 interface PlayerPosition {
   top: string;
   left: string;
 }
 
-// טיפוס לנתוני השחקן
+
 interface PlayerProfile {
   playerMainPosition: string;
 }
 
-// מיפוי של כל העמדות למיקומים על המגרש
+
 const positions: Record<string, PlayerPosition> = {
   "Goalkeeper": { top: "90%", left: "50%" },
   "Centre-Back": { top: "60%", left: "30%" },
@@ -28,23 +28,23 @@ const positions: Record<string, PlayerPosition> = {
 
 const FootballField: React.FC<PlayerProfile> = ({ playerMainPosition }) => {
   const position = `${playerMainPosition}`;
-  const playerStyle = positions[position] || { top: "50%", left: "50%" }; // Default to center
+  const playerStyle = positions[position] || { top: "50%", left: "50%" }; 
 
   return (
     <div style={{ position: "relative", width: "400px", height: "500px", backgroundColor: "green", border: "2px solid black" }}>
-      {/* קווים של המגרש */}
+ 
       <div style={{ position: "absolute", top: "0", left: "0", width: "100%", height: "100%", border: "1px solid white" }}></div>
-      {/* קו מחצית */}
-      <div style={{ position: "absolute", top: "50%", left: "0", width: "100%", height: "1px", backgroundColor: "white" }}></div>
-      {/* קו שער - צידו השמאלי של המגרש */}
-      <div style={{ position: "absolute", top: "0", left: "0", width: "1px", height: "100%", backgroundColor: "white" }}></div>
-      {/* קו שער - צידו הימני של המגרש */}
-      <div style={{ position: "absolute", top: "0", right: "0", width: "1px", height: "100%", backgroundColor: "white" }}></div>
-      {/* חוג עיגול מרכזי */}
-      <div style={{ position: "absolute", top: "50%", left: "50%", width: "60px", height: "60px", borderRadius: "50%", border: "1px solid white", transform: "translate(-50%, -50%)" }}></div>
-      {/* קווים נוספים אם נדרש */}
 
-      {/* השחקן */}
+      <div style={{ position: "absolute", top: "50%", left: "0", width: "100%", height: "1px", backgroundColor: "white" }}></div>
+
+      <div style={{ position: "absolute", top: "0", left: "0", width: "1px", height: "100%", backgroundColor: "white" }}></div>
+
+      <div style={{ position: "absolute", top: "0", right: "0", width: "1px", height: "100%", backgroundColor: "white" }}></div>
+
+      <div style={{ position: "absolute", top: "50%", left: "50%", width: "60px", height: "60px", borderRadius: "50%", border: "1px solid white", transform: "translate(-50%, -50%)" }}></div>
+
+
+
       <div
         style={{
           position: "absolute",
