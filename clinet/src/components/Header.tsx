a402c810c3073af80352141d0d256b65361b75a2
MaorShmueli55 <maorshmueli5@gmail.com>
2025-01-19 19:04:03 +0200

    344

--- clinet/src/components/Header.tsx
+++ clinet/src/components/Header.tsx
@@ -74,18 +74,17 @@ const Header = () => {
           <MdPersonSearch className="text-[#00193f] text-[20px] transition-all duration-300 group-hover:text-white" />
         </button>
       </div>
-<<<<<<< HEAD
-      <div className="lg:ml-[28px] sm:ml-[2px] gap-[7px] rounded-[4px] flex items-center justify-center">
+
+
+
+
+
+        <div className="lg:ml-[28px] sm:ml-[2px] gap-[7px] rounded-[4px] flex items-center justify-center">
+        {user ? (
+          <Link to={`/favorite/${user?.id}`}>
         <button className="bg-white p-[6px] rounded-[4px] flex items-center justify-center transition-all duration-300 hover:bg-[#00193f] group">
           <BsBookmarkStarFill className="text-[#00193f] text-[18px] transition-all duration-300 group-hover:text-white" />
         </button>
-=======
-      <div className="ml-[28px] gap-[7px] rounded-[4px] flex items-center justify-center">
-        {user ? (
-          <Link to={`/favorite/${user?.id}`}>
-            <button className="bg-white p-[6px] rounded-[4px] flex items-center justify-center transition-all duration-300 hover:bg-[#00193f] group">
-              <BsBookmarkStarFill className="text-[#00193f] text-[18px] transition-all duration-300 group-hover:text-white" />
-            </button>
           </Link>
         ) : (
           <Link to={`/favorite/user-logout`}>
@@ -94,7 +93,6 @@ const Header = () => {
             </button>
           </Link>
         )}
->>>>>>> 9fa548c3bccc78cd494733b3f48a5d30909ec94f
         <button className="bg-white p-[6px] rounded-[4px] flex items-center justify-center transition-all duration-300 hover:bg-[#00193f] group">
           <BiSolidShieldPlus className="text-[#00193f] text-[18px] transition-all duration-300 group-hover:text-white" />
         </button>
